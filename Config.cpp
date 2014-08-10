c3f619f96fbc45d827e5be88d4f0651ca52f9ee1
plushmonkey <plushmonkey.ss@gmail.com>
2014-08-10 18:12:29 -0400

    Bombing, configuration, and more.

---
+++ Config.cpp
@@ -0,0 +1,83 @@
+#include "Config.h"
+#include <regex>
+#include <fstream>
+#include <tchar.h>
+#include <algorithm>
+
+const TCHAR regex_str[] = _T("^\\s*([a-zA-Z_]\\w*)\\s*=\\s*(.+?)\\s*$");
+
+namespace Convert {
+    template<>
+    int Get(const tstring& val) {
+        if (val.length() == 0) return 0;
+        return _tstoi(val.c_str());
+    }
+
+    template<>
+    unsigned int Get(const tstring& val) {
+        if (val.length() == 0) return 0;
+        return _tstoi(val.c_str());
+    }
+
+    template<>
+    tstring Get(const tstring& val) {
+        return val;
+    }
+
+    template<>
+    bool Get(const tstring& val) {
+        if (val.length() == 0) return false;
+
+        tstring out;
+        out.resize(val.length());
+
+        std::transform(val.begin(), val.end(), out.begin(), _totlower);
+
+        return out.compare(_T("true")) == 0 || out.compare(_T("t")) == 0;
+    }
+
+    template<>
+    TCHAR Get(const tstring& val) {
+        if (val.length() == 0) return '\0';
+        return val.at(0);
+    }
+}
+
+void Config::Set(const tstring& var, const tstring& val) {
+    m_Variables[var] = val;
+}
+
+bool Config::Load(const tstring& filename) {
+    tifstream file;
+    file.open(filename, std::ios::in);
+
+    tregex regex(regex_str);
+
+    if (!file.is_open())
+        return false;
+
+    tstring data;
+
+    file.seekg(0, std::ios::end);
+    data.reserve(static_cast<unsigned int>(file.tellg()));
+    file.seekg(0, std::ios::beg);
+
+    data.assign((std::istreambuf_iterator<TCHAR>(file)), std::istreambuf_iterator<TCHAR>());
+
+    tsregex_iterator begin(data.begin(), data.end(), regex);
+    tsregex_iterator end;
+
+    for (tsregex_iterator iter = begin; iter != end; ++iter) {
+        tsmatch match = *iter;
+
+        tstring var = match[1];
+
+        var.resize(match[1].length());
+
+        std::transform(var.begin(), var.end(), var.begin(), _totlower);
+
+        m_Variables[var] = match[2];
+    }
+
+    return true;
+}
---
+++ Config.h
@@ -0,0 +1,34 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#include <map>
+#include <string>
+#include "Common.h"
+#include <tchar.h>
+
+namespace Convert {
+    template <typename T>
+    T Get(const tstring& val);
+}
+
+class Config {
+private:
+    std::map<tstring, tstring> m_Variables;
+
+public:
+    bool Load(const tstring& filename);
+
+    template <typename T>
+    T Get(const tstring& var) const {
+        tstring out;
+        out.resize(var.length());
+        for (unsigned int i = 0; i < var.length(); i++)
+            out[i] = _totlower(var[i]);
+
+        return Convert::Get<T>(m_Variables.at(out));
+    }
+
+    void Set(const tstring& var, const tstring& val);
+};
+
+#endif
